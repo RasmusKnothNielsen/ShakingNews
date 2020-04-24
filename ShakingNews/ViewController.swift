3a747a2d9e4018baee8ed8209f346a5dc0213cdb
Rasmus Knoth Nielsen <Rasmus.knoth@gmail.com>
2020-04-24 17:28:41 +0200

    Initial Commit

---
+++ ShakingNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShakingNews
+//
+//  Created by Rasmus Knoth Nielsen on 24/04/2020.
+//  Copyright © 2020 Rasmus Knoth Nielsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
