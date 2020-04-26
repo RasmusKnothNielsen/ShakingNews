987e52a6eb8ec12b15aa9dc298a4e0c425a02c26
Rasmus Knoth Nielsen <Rasmus.knoth@gmail.com>
2020-04-26 12:49:14 +0200

    Initial Commit

--- ShakingNews/ViewController.swift
+++ ShakingNews/ViewController.swift
@@ -7,14 +7,28 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var webKitView: WKWebView!
+    
+    let arrayOfSites = ["DR":"https://www.dr.dk", "Reddit": "https://www.reddit.com", "JP": "https://www.jp.dk", "Min Mave": "https://www.min-mave.dk", "Heste Nettet": "https://www.heste-nettet.dk"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let url = arrayOfSites.randomElement()!
+        label.text = url.key
+        webKitView.load(URLRequest(url: (URL(string: url.value) ?? URL(string: "www.google.com"))!))
     }
 
-
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        print("Phone was shaken")
+        let url = arrayOfSites.randomElement()!
+        print("URL: \(url)")
+        label.text = url.key
+        webKitView.load(URLRequest(url: (URL(string: url.value) ?? URL(string: "https://www.google.com"))!))
+    }
 }
 
